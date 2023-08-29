866f34155d57737226552d213504f32b55858e0a
ryoogawa1213 <s195013@matsu.shimane-u.ac.jp>
2023-08-29 16:15:11 +0000

    first commit

---
+++ Practice01-1.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Hello World!\n";
+    
+    $string = "string";
+    $integer = 1;
+    $arrary = array(1,2,3);
+    $boolean = true;
+    $NULL = NULL;
+    
+    echo gettype($string)."\n";
+    echo gettype($integer)."\n";
+    echo gettype($array)."\n";
+    echo gettype($boolean)."\n";
+    echo gettype($NULL)."\n";
+?>
